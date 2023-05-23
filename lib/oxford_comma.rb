c419db6c3516120b8b068b08fd296f066f0afe16
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-23 17:43:42 +0300

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+    case array.size
+    when 1
+      return array[0]
+    when 2
+      return array.join(" and ")
+    else
+      last_element = array.pop
+      return "#{array.join(", ")}, and #{last_element}"
+    end
+  end
+  
 
 end
